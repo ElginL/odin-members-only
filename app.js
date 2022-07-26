07228e7694ef7ab1a7da1bc6acd83a35f8111d64
ElginL <elginlee_2000@hotmail.com>
2022-07-26 23:20:54 +0800

    add express app

---
+++ app.js
@@ -0,0 +1,46 @@
+const createError = require('http-errors');
+
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// set up connection with mongodb
+mongoose.connect(process.env.mongodbURI, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "mongo connection error"));
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(session({ secret: process.env.sessionSecret, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });  
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
